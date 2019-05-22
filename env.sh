bae48b4edda36be616b297ffe97d66bc5ad840e1
bay1 <328588917@qq.com>
2019-05-23 01:27:06 +0800

    add bay1sops

---
+++ env.sh
@@ -0,0 +1,7 @@
+export DB_NAME="bay1sops"
+export DB_USERNAME="root"
+export DB_PASSWORD="root"
+export DB_HOST="localhost"
+export DB_PORT="3306"
+
+
--- gcloud/taskflow3/views.py
+++ gcloud/taskflow3/views.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community
-Edition) available.
+Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community Edition) available.
 Copyright (C) 2017-2019 THL A29 Limited, a Tencent company. All rights reserved.
-Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
 http://opensource.org/licenses/MIT
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
-
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+""" # noqa
+"""
 @summary: 任务流程实例views
 """
-
-from django.shortcuts import render
+from common.mymako import render_mako_context
 
 
 def home(request, biz_cc_id):
@@ -28,4 +24,4 @@ def home(request, biz_cc_id):
         "app_id": "",
         "template_id": request.GET.get('template_id', ''),
     }
-    return render(request, 'core/base_vue.html', ctx)
+    return render_mako_context(request, '/core/base_vue.html', ctx)
